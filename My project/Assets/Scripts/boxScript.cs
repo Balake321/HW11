e2f9b55c534e8f02c89faf4dbf6381bec80d14ff
Blake <blake.breon@cuw.edu>
2022-04-11 12:36:17 -0500

    class

--- My project/Assets/Scripts/boxScript.cs
+++ My project/Assets/Scripts/boxScript.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity
 
 public class boxScript : MonoBehaviour
 {
     private int count;
+    public GameObject thePlayer;
 
     // Start is called before the first frame update
     void Start() //like a constructor
@@ -12,21 +14,23 @@ public class boxScript : MonoBehaviour
         count = 0;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag.Equals("Player"))
         {
+            CORE.display()
             count++
-            if(count = 3)
+            if(count == 3)
             {
-                this.gameObject.SendMessage();
+                this.gameObject.SendMessage("DoSomething");
+                Destory(this.gameObject);
             }
-            print("box collision enter" + count);
         }
     }
     // Update is called once per frame
     void Update()
     {
-        
+        playerPosition = thePlayer.transform.position;
+        rb.velocity = new Vector3(playerPosition.x * speed, playerPosition.y * speed, playerposition.z * speed);
     }
 }
